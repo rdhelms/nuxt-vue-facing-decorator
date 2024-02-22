b3c79bcad48a3f9ebcb4b0ade4775ba4c3a44f8c
Robby Helms <robby@pocketprep.com>
2024-02-22 08:50:39 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, toNative } from 'vue-facing-decorator';
+
+@Component({})
+class MainApp extends Vue {}
+
+export default toNative(MainApp);
+</script>
